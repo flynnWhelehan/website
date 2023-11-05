3c332580c42d4f8b9c60b9f14139567f94b472d5
Flynn <flynn.whelehan@gmail.com>
2023-11-05 00:53:19 +0000

    Use React strategies like routing and components

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -1,36 +1,40 @@
 import loadingIcon from './images/loadingIcon.svg';
 import './App.css';
 import React from 'react';
-/*
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
-*/
 
 function App() {
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <img src={loadingIcon} className="App-logo" alt="loadingIcon" />
-
-        <p>
-          Website in progress...
-        </p>
-
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/flynnwhelehan/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Connect with Flynn
-        </a>
-      </header>
-
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" component={About} />
+          <Route path="/" element={
+            <>
+              <header className="App-header">
+                <img src={loadingIcon} className="App-logo" alt="loadingIcon" />
+                <p>
+                  Website in progress...
+                </p>
+                <a
+                  className="App-link"
+                  href="https://www.linkedin.com/in/flynnwhelehan/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Connect with Flynn
+                </a>
+              </header>
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
