2d6ac2e2d4eed296c9e14ec9a257b408f6ec057c
Flynn <flynn.whelehan@gmail.com>
2023-11-05 00:03:04 +0000

    Namespace for implementing react router

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -1,5 +1,13 @@
 import loadingIcon from './images/loadingIcon.svg';
 import './App.css';
+import React from 'react';
+/*
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import Navbar from './components/Navbar';
+*/
 
 function App() {
   return (
@@ -27,3 +35,5 @@ function App() {
 }
 
 export default App;
+
+
