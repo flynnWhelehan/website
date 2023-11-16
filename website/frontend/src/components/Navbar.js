ca250ce274121dcf47c9c5e5850a6fb20a3006be
Flynn <flynn.whelehan@gmail.com>
2023-11-16 21:25:28 +0000

    Titles merge into navbar upon scrolling past them. (sticky titles - helps know where you are)

--- website/frontend/src/components/Navbar.js
+++ website/frontend/src/components/Navbar.js
@@ -6,6 +6,7 @@ import FwLogo from '../images/favicon/favicon-32x32.png';
 function Navbar() {
   const [shrink, setShrink] = useState(false);
   const [opaque, setOpaque] = useState(false);
+  const [currentTitle, setCurrentTitle] = useState('');
 
   const handleScroll = () => {
     if (window.scrollY > 80) {
@@ -14,6 +15,22 @@ function Navbar() {
     } else {
       setShrink(false);
       setOpaque(false);
+    }
+
+    // Get all title elements
+    const titles = Array.from(document.querySelectorAll('.title'));
+    let found = false;
+    for (let i = titles.length - 1; i >= 0; i--) {
+      const rect = titles[i].getBoundingClientRect();
+      if (rect.top <= 0) {
+        setCurrentTitle(titles[i].textContent);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      setCurrentTitle('');
     }
   };
 
@@ -32,6 +49,9 @@ function Navbar() {
       <div className="links-container">
         <Link to="/about" className="navbar-link">About</Link>
         <a href="https://www.linkedin.com/in/flynnwhelehan/" className="navbar-link">Contact</a>
+      </div>
+      <div className="current-title-container">
+        <span className="current-title">{currentTitle}</span>
       </div>
     </div>
   );
