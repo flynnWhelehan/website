b64b5a1213873ab239b79b2a80b9236367c1e2f9
Flynn <flynn.whelehan@gmail.com>
2023-11-16 22:41:49 +0000

    Update navbar name

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
