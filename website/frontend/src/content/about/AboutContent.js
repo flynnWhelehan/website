898991f408641a399578c79455ac1c944c877900
Flynn <flynn.whelehan@gmail.com>
2023-11-16 20:46:17 +0000

    Setup content directory and import to about page.

---
+++ website/frontend/src/content/about/AboutContent.js
@@ -0,0 +1,8 @@
+const aboutContent = {
+    title: "Flynn's about page",
+    intro: "All about FW. Web site in progress...",
+    about: "I am actively working on a portfolio-based website. I will primarily focus on frontend technologies to begin. It will be a place to share my creative side and showcase some of what I get up to, all while developing new skills through using some unfamiliar tools and techniques. I have lots of creative ideas for this project, and will steadily progress with it as I find the time.",
+    mission: "Our mission is to provide high quality content and services to our users.",
+};
+
+export default aboutContent;
--- website/frontend/src/pages/About.js
+++ website/frontend/src/pages/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Container, Typography, Box, Divider } from '@mui/material';
+import aboutContent from '../content/about/AboutContent';
+
 
 function About() {
   return (
@@ -11,30 +13,29 @@ function About() {
     >
       <Container>
         <Helmet>
-          <title>Flynn's about page</title>
-          <meta name="All about FW." content="Web site in progress..." />
+          <title>{aboutContent.title}</title>
+          <meta name="All about FW." content={aboutContent.intro} />
         </Helmet>
+
         <Box my={4}>
           <Typography variant="h4" component="h1" gutterBottom>
             About
           </Typography>
           <Typography variant="body1" gutterBottom>
-            Welcome to the about page of FW's website. This website is currently under development.
+            {aboutContent.about}
           </Typography>
+
           <Divider variant="middle" />
+
           <Typography variant="h5" component="h2" gutterBottom>
             Our Mission
           </Typography>
           <Typography variant="body1" gutterBottom>
-            Our mission is to provide high quality content and services to our users.
+            {aboutContent.mission}
           </Typography>
+
           <Divider variant="middle" />
-          <Typography variant="h5" component="h2" gutterBottom>
-            Our Team
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            We have a dedicated team of professionals working hard to deliver the best experience to our users.
-          </Typography>
+
         </Box>
       </Container>
     </Box>
