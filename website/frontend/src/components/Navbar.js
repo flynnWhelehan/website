19bf86cd173d77d6a98b821d08bf5a4452ea0133
Flynn <flynn.whelehan@gmail.com>
2023-11-16 21:08:05 +0000

    navbar is dynamic with page movement, no longer a solid block on screen

--- website/frontend/src/components/Navbar.js
+++ website/frontend/src/components/Navbar.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/Navbar.css';
 import { Link } from 'react-router-dom';
 import FwLogo from '../images/favicon/favicon-32x32.png';
 
 function Navbar() {
+  const [shrink, setShrink] = useState(false);
+  const [opaque, setOpaque] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 80) {
+      setShrink(true);
+      setOpaque(true);
+    } else {
+      setShrink(false);
+      setOpaque(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${shrink ? 'shrink' : ''} ${opaque ? 'opaque' : ''}`}>
       <Link to="/">
-        <img src={FwLogo} alt="FW Logo / Home" className="logo" />
+        <img src={FwLogo} alt="FW Logo / Home" className={`logo ${shrink ? 'shrink' : ''}`} />
       </Link>
-      <Link to="/about">About</Link>
-      <a href="https://www.linkedin.com/in/flynnwhelehan/">Contact</a>
+      <div className="links-container">
+        <Link to="/about" className="navbar-link">About</Link>
+        <a href="https://www.linkedin.com/in/flynnwhelehan/" className="navbar-link">Contact</a>
+      </div>
     </div>
   );
 }
