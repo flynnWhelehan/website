f6f0f15d25911c4e97421324a83910ef9798d1fc
Flynn <flynn.whelehan@gmail.com>
2023-11-17 23:16:48 +0000

    Add basic footer

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           <Route path="/website" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
-
+        <Footer/>
       </div>
     </Router>
   );
