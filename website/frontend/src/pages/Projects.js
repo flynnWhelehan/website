86cce31e69ee3b5763c94d1dd90f370ae1e52813
Flynn <flynn.whelehan@gmail.com>
2024-06-09 00:53:44 +0100

    Add a bunch of project/experience tiles

--- website/frontend/src/pages/Projects.js
+++ website/frontend/src/pages/Projects.js
@@ -3,10 +3,20 @@ import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import './Styles/Projects.css';
 import myairopsLogo from '../images/myairops-logo.jpeg';
+import canBusDiagram from '../images/can-bus-diagram.png';
+import royalHackaway from '../images/royal-hackaway.png';
+import framaCLogo from '../images/frama-c.jpg';
+import zooplaLogo from '../images/zoopla-logo.jpg';
+import strandPalaceLogo from '../images/strand-palace.jpg';
 
 function Projects() {
     const projects = [
         { name: 'Myairops Industrial Placement', thumbnail: myairopsLogo, link: '/myairops-project'},
+        { name: 'Hackathon - Royal Hackaway v7', thumbnail: royalHackaway, comingSoon: true},
+        { name: 'Formal Software Verification', thumbnail: framaCLogo, comingSoon: true},
+        { name: 'Vehicle Accident Scene Reconstruction Research', thumbnail: canBusDiagram, comingSoon: true},
+        { name: 'Zoopla - IT Support', thumbnail: zooplaLogo, comingSoon: true},
+        { name: 'Strand Palace Hotel - IT Support', thumbnail: strandPalaceLogo, comingSoon: true},
     ];
 
     return (
